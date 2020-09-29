95b5b3e7151abd7ef07bd9c2fb51bf147cacbe07
LUISARTURCORREIA <72110275+LUISARTURCORREIA@users.noreply.github.com>
2020-09-29 20:05:15 -0300

    Exercício 02 - Feito em 26/08/2019

---
+++ Exercicios02/Dominio/cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios02.Dominio
+{
+    public class Cliente
+    {
+        public string nome;
+        public string codigo;
+        public string rg, cpf;
+        public string Nascimento;
+        public string idade;
+
+        public void Setrg(string rg)
+        {
+            this.rg = rg;
+        }
+        public void Setcpf(string cpf)
+        {
+            this.cpf = cpf;
+        }
+
+        public void SetNascimento(string Nascimento)
+        {
+            this.Nascimento = Nascimento;
+        }
+        public void Setidade(string idade)
+        {
+            this.idade = idade;
+        }
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public void SetCodigo(string codigo)
+        {
+            this.codigo = codigo;
+        }
+    }
+}
---
+++ Exercicios02/Program.cs
@@ -0,0 +1,33 @@
+﻿using Exercicios02.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Cliente objcliente = new Cliente();
+            objcliente.nome = "João";
+            objcliente.codigo = "0";
+            objcliente.idade = "18";
+            objcliente.cpf = "08717961980";
+            objcliente.rg = "154794694613648";
+            objcliente.Nascimento = "25/07/2002";
+            Console.ReadLine();
+
+            Cliente objcliente2 = new Cliente();
+            objcliente.nome = "josé";
+            objcliente.codigo = "2";
+            objcliente.idade = "22";
+            objcliente.cpf = "061496149498";
+            objcliente.rg = "989848949849469";
+            objcliente.Nascimento = "22/07/2002";
+        }
+    }
+}
